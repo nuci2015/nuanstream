565f88d9bdedd17153d2d1dabf869111606e91d3
nuci2015 <nunziopetrone2012@gmail.com>
2026-08-03 08:19:08 +0200

    Create build.gradles.kts

---
+++ build.gradles.kts
@@ -0,0 +1,5 @@
+plugins {
+    id("com.android.application") version "8.1.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.lagradost.cloudstream3.gradle") version "1.0.9" apply false
+}
