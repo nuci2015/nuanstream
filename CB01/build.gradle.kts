6514cd6cebad0defa79206f7d673a6806ab28178
nuci2015 <nunziopetrone2012@gmail.com>
2026-08-03 05:13:20 +0200

    Update build.gradle.kts

--- CB01/build.gradle.kts
+++ CB01/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     cloudstream("com.lagradost:cloudstream3:pre-release")
 
     implementation(kotlin("stdlib"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
     implementation("com.github.Blatzar:NiceHttp:0.4.11")
     implementation("org.jsoup:jsoup:1.18.3")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
