05b6ff43025e70d19146fcf7bdd8e93ad7b208f6
nuci2015 <nunziopetrone2012@gmail.com>
2026-08-03 06:28:41 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,12 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+        gradlePluginPortal()
+    }
+}
+
 plugins {
     id("com.android.application") version "8.1.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
@@ -9,21 +18,5 @@ subprojects {
         google()
         mavenCentral()
         maven { url = uri("https://jitpack.io") }
-    }
-}
-
-allprojects {
-    afterEvaluate {
-        project.extensions.findByName("android")?.let { android ->
-            android.compileSdkVersion(34)
-            android.defaultConfig {
-                minSdk(21)
-                targetSdk(34)
-            }
-            android.compileOptions {
-                sourceCompatibility = JavaVersion.VERSION_17
-                targetCompatibility = JavaVersion.VERSION_17
-            }
-        }
     }
 }
