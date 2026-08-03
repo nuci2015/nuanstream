b9fe2ed6b6423288bb04481be706e09b81dcf80c
nuci2015 <nunziopetrone2012@gmail.com>
2026-08-03 04:22:51 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:8.1.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
-        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
+        classpath("com.github.recloudstream:gradle:32895ae")
     }
 }
 
