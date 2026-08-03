eae86a952de08b5d51ad86828df2072b210cdae9
nuci2015 <nunziopetrone2012@gmail.com>
2026-08-03 05:21:25 +0200

    Create CB01Plugin.kt

---
+++ CB01/src/main/kotlin/it/dogior/hadEnough/CB01Plugin.kt
@@ -0,0 +1,12 @@
+package it.dogior.hadEnough
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class CB01Plugin : Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(CB01())
+    }
+}
