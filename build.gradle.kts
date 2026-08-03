b361758036dd28af3679134414dea5a0d0d097f5
nuci2015 <nunziopetrone2012@gmail.com>
2026-08-03 06:34:15 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,5 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-        maven { url = uri("https://jitpack.io") }
-    }
-}
-
 plugins {
     id("com.android.application") version "8.1.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    id("com.lagradost.cloudstream3.gradle") version "1.0.9"
-}
-
-subprojects {
-    repositories {
-        google()
-        mavenCentral()
-        maven { url = uri("https://jitpack.io") }
-    }
+    id("com.lagradost.cloudstream3.gradle") version "1.0.9" apply false
 }
