d84ea6bc408dc399e0be00c1dbbdac54b7fb7c39
nuci2015 <nunziopetrone2012@gmail.com>
2026-08-03 03:58:35 +0200

    Update build.gradle.kts

--- CB01/build.gradle.kts
+++ CB01/build.gradle.kts
@@ -4,6 +4,24 @@ plugins {
     id("com.lagradost.cloudstream3.gradle")
 }
 
+android {
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 34
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
 cloudstream {
     description = "Provider CB01"
     authors = listOf("Nunciè")
